316d8464b47328f2f4fe81da0ddefd84c61602c7
BaekSH <baek6342@naver.com>
2023-04-12 01:34:32 +0900

    04011

---
+++ app/src/main/java/com/example/rescuesupport/Fragment_menu_home.java
@@ -0,0 +1,19 @@
+package com.example.rescuesupport;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.viewmodel.CreationExtras;
+
+public class Fragment_menu_home extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu_home, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/rescuesupport/Fragment_table_social.java
@@ -0,0 +1,22 @@
+package com.example.rescuesupport;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_table_social extends Fragment {
+
+    public Fragment_table_social() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_table_social, container, false);
+    }
+
+}
